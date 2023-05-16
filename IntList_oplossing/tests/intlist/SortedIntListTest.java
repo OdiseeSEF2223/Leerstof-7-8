f17d32157a5aba93ace143fd5b9c598f8168e97e
maartentroost <maarten.troost@odisee.be>
2023-05-16 22:24:47 +0200

    intlist oplossing

---
+++ IntList_oplossing/tests/intlist/SortedIntListTest.java
@@ -0,0 +1,58 @@
+package intlist;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortedIntListTest {
+
+    final int[] values = {5,4,3,2,1,8,9,};
+    final int[] sortedValues = {1,2,3,4,5,8,9,};  //same list but ordered
+    SortedIntList list;
+
+    private void addAllValues(){
+        for(final int value : values){
+            list.add(value);
+        }
+    }
+
+    @BeforeEach
+    public void setup(){
+        list = new SortedIntList();
+    }
+
+    // Tests zelfde als gewone IntList
+    @Test
+    void findMin_WithoutAddedValues_ReturnsNull() {
+        // Act
+        final Integer min = list.findMin();
+        // Assert
+        assertNull(min);
+    }
+
+    @Test
+    void findMin_WithAddedValues_ReturnsLowest1() {
+        // Arrange
+        addAllValues();
+        // Act
+        final Integer min = list.findMin();
+        // Assert
+        assertEquals(1, min);
+    }
+
+    //Andere functionaliteit
+    //getallen worden gesorteerd bijgehouden
+    @Test
+    void remove_WithValuesAdded_AllSortedValuesReturnedInOrder() {
+        // Arrange
+        addAllValues();
+        // Act
+        // Assert
+        for(final int value : sortedValues) {
+            assertEquals(value, list.remove(0));
+        }
+        assertNull(list.remove(0)); // Are there no Integers left behind?
+    }
+
+}
