b57f30cae8240ddea0fffb9841e6b051f968a140
maartentroost <maarten.troost@odisee.be>
2023-05-21 22:13:01 +0200

    Les5

---
+++ oefCompositionBank_opgave/src/oefBank/Bank.java
@@ -0,0 +1,43 @@
+package oefBank;
+
+import java.util.ArrayList;
+
+/* Dit is nog de oude Bank met aggregatie */
+public class Bank {
+
+    private ArrayList<Account> accounts = new ArrayList<Account>();
+
+    public void openAccount(Account account){
+        if(!accounts.contains(account)) {
+            accounts.add(account);
+        }
+    }
+
+    private int numAccounts(){
+        return accounts.size();
+    }
+
+    public void closeAccount(Account account){
+        accounts.remove(account);
+    }
+
+
+    public String toString() {
+        String str=String.format("Bank managing %d accounts:\n",numAccounts());
+        for (Account account : accounts) {
+            str+=account.toString()+"\n";
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        //Hoe een Bank met composition zou gebruikt kunnen worden
+        Bank myBank=new Bank();
+/*        myBank.openAccount();
+        myBank.openAccount();
+        myBank.openAccount();
+        myBank.withdraw(1,20); //should print Cannot withdraw money that is not present
+*/
+        System.out.println(myBank.toString()); //should print 3 accounts with 0 balance
+    }
+}
---
+++ oefCompositionBank_opgave/src/readme.md
@@ -0,0 +1,10 @@
+# Opgave 
+Maak van de **Bank – Account** relatie een compositie.
+## Requirements / functionaliteiten:
+- [ ] Gebruik composition
+- [ ] Account class wijzigt niet
+- [ ] Een nieuw account kan geopend worden (bij een bank)
+- [ ] Een account kan gesloten worden
+- [ ] Geld kan afgehaald worden (withdraw)
+
+Je mag (private) help methods maken.
