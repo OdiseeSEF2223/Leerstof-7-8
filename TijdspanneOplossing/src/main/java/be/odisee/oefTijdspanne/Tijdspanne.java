e98e7142ae83d6edede5ba174fcb9be1fc2cf620
maartentroost <maarten.troost@odisee.be>
2023-05-23 10:37:25 +0200

    tijdspanne

---
+++ TijdspanneOplossing/src/main/java/be/odisee/oefTijdspanne/Tijdspanne.java
@@ -0,0 +1,39 @@
+package be.odisee.oefTijdspanne;
+
+/** Onthoudt een tijdspanne als een aantal minuten */
+public class Tijdspanne {
+    protected int minuten;
+    public Tijdspanne(int minuten) {
+        this.minuten = minuten;
+    }
+    public int getMinuten() {
+        return minuten;
+    }
+
+    /**
+     * Tel 2 tijdspannes bij elkaar op: de eigen tijd plus de tijd van de andere
+     * @param andere andere tijdspanne
+     */
+    public void bijTellen(Tijdspanne andere) {
+        minuten+=andere.getMinuten();
+    }
+}
+/** Onthoudt een tijdspanne als een aantal minuten en seconden */
+class PreciezeTijdspanne extends Tijdspanne {
+    protected int seconden;
+    public int getSeconden() {
+        return seconden;
+    }
+    public PreciezeTijdspanne(int minuten, int seconden) {
+        super(minuten);
+        this.seconden = seconden;
+    }
+    /**
+     * Tel 2 tijdspannes bij elkaar op: de eigen tijd plus de tijd van de andere
+     * @param andere andere tijdspanne
+     */
+    public void bijTellen(PreciezeTijdspanne andere) {
+        super.bijTellen(andere);
+        this.seconden+=andere.getSeconden();
+    }
+}
